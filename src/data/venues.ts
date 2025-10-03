e6963f16038631b8321ba027803c39f0a7448370
Michal Jankura <michal.jankura@student.tuke.sk>
2025-10-03 17:51:07 +0200

    new clock destilery

--- src/data/venues.ts
+++ src/data/venues.ts
@@ -327,13 +327,13 @@ export const venues: Venue[] = [
   rating: 4.3,
   ratingCount: 154,
     weeklyHours: {
-      mon: { open: '10:00', close: '20:00' },
+      mon: null,
       tue: { open: '10:00', close: '20:00' },
       wed: { open: '10:00', close: '20:00' },
       thu: { open: '10:00', close: '20:00' },
       fri: { open: '10:00', close: '20:00' },
-      sat: { open: '10:00', close: '21:00' },
-      sun: { open: '10:00', close: '21:00' }
+      sat: { open: '10:00', close: '20:00' },
+      sun: { open: '10:00', close: '20:00' }
     },
     website: 'https://www.castledistillery.sk/',
     facebook: 'https://www.facebook.com/castledistillery/',
